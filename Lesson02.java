47b7550ac78e0774ff2044f0f0427be70494e456
mashiken <tatsumikogorou@gmail.com>
2020-11-13 09:05:53 +0900

    variable learn

---
+++ Lesson02.java
@@ -0,0 +1,13 @@
+public class Lesson02 {
+    public static void main(String[] args){
+        int var;
+        var = 1;
+        int Var = 2;
+        System.out.println(var);
+        System.out.println(Var);
+
+    }
+
+
+
+}
