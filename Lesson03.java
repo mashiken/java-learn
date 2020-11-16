31d5a1474967739573c397e1193f925dce416daa
mashiken <tatsumikogorou@gmail.com>
2020-11-16 23:29:06 +0900

    variable types learn

--- Lesson03.java
+++ Lesson03.java
@@ -10,10 +10,19 @@ public class Lesson03 {
         System.out.println(var03);
         System.out.println(var04);
 
-        
+        double var05 = 1.23456789;
+        System.out.println(var05);
 
+        char var06 = 'c';
+        String var07 = "hello,World";
+        System.out.println(var06);
+        System.out.println(var07);
+
+        int var_a = 10;
+        int var_b = 1;
+        boolean var_bool;
+
+        var_bool = (var_a > var_b);
+        System.out.println(var_bool);
     }
-
-
-
 }
