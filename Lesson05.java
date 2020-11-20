306632e13f66734d4ba6825727e02386e04a8299
mashiken <tatsumikogorou@gmail.com>
2020-11-21 05:43:20 +0900

    operate

--- Lesson05.java
+++ Lesson05.java
@@ -13,6 +13,26 @@ public class Lesson05 {
         System.out.println(x > y);
         System.out.println(x < y);
         System.out.println(x <= z);
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+
+        int a = 8;
+        int b = 3;
+        System.out.println(a >= 5 && a <= 10);
+        System.out.println(b >= 5 && b <= 10);
+        
+        int c = 8;
+        int d = 3;
+        System.out.println(c == 3 || d == 3);
+        System.out.println(c == 1 || d == 1);
+
+        System.out.println(a+=c);
+        System.out.println(d+=b);
+        c++;
+        b--;
+        System.out.println(c);
+        System.out.println(b);
     }
 
 }
